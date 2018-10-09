956ed0728d0cfbb8ee78e14241b99c6f9516562c
魏霞进 <weixiajin@jplay.com>
2018-10-09 19:32:30 +0800

    1.提交个人信息界面

---
+++ TVBox/app/src/main/java/com/apemoon/tvbox/ui/adapter/personalCenter/SemestersAdapter.java
@@ -0,0 +1,45 @@
+package com.apemoon.tvbox.ui.adapter.personalCenter;
+
+import android.view.View;
+
+import com.apemoon.tvbox.R;
+import com.apemoon.tvbox.entity.userCenter.UserRecordInfoEntity;
+import com.apemoon.tvbox.entity.userCenter.UserSemstersEntity;
+import com.apemoon.tvbox.ui.view.ItemLinearLayout;
+import com.apemoon.tvbox.utils.AnimationUtil;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+/**
+ * Created by water on 2018/8/28/028.
+ * des：
+ */
+
+public class SemestersAdapter extends BaseQuickAdapter<UserSemstersEntity.SemstersBean,BaseViewHolder> {
+
+    public SemestersAdapter() {
+        super(R.layout.item_personl_santion);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, UserSemstersEntity.SemstersBean item) {
+        if (item != null) {
+//            ((ItemLinearLayout)helper.getView(R.id.item_school)).setRightText(item.getSchoolName());
+//            ((ItemLinearLayout)helper.getView(R.id.item_time)).setRightText(item.getStartTime());
+//            ((ItemLinearLayout)helper.getView(R.id.item_santion)).setRightText(item.get());
+//            ((ItemLinearLayout)helper.getView(R.id.item_info)).setRightText(item.get());
+
+
+
+        }
+
+        helper.getView(R.id.ll_notice).setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                AnimationUtil.setTextAnimation(view,hasFocus,1.01f,1.01f,1.0f,1.0f);
+
+            }
+        });
+    }
+
+}
